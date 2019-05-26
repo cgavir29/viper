28ea4e1e1d195d1f2a60fd4db9214bbc3fa284fe
sarredondq <sarredondq@eafit.edu.co>
2019-05-25 23:59:33 -0500

    arreglos visuales

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,7 +18,23 @@ class UpdateTeacherVenueForm(forms.ModelForm):
 
 
 class TeacherScheduleForm(forms.ModelForm):
-
+    TIME_CHOICES = (
+        ('6:00 - 7:00'),
+        ('7:00 - 8:00'),
+        ('8:00 - 9:00'),
+        ('9:00 - 10:00'),
+        ('10:00 - 11:00'),
+        ('11:00 - 12:00'),
+        ('12:00 - 13:00'),
+        ('13:00 - 14:00'),
+        ('14:00 - 15:00'),
+        ('15:00 - 16:00'),
+        ('16:00 - 17:00'),
+        ('17:00 - 18:00'),
+        ('18:00 - 19:00'),
+        ('19:00 - 20:00'),
+        ('20:00 - 21:00')
+    )
     class Meta:
         model = Schedule
         fields = ['monday', 'tuesday', 'wednesday',
@@ -29,7 +45,23 @@ class TeacherScheduleForm(forms.ModelForm):
 
 
 class TeacherScheduleCreateForm(forms.ModelForm):
-
+    TIME_CHOICES = (
+        ('6:00 - 7:00'),
+        ('7:00 - 8:00'),
+        ('8:00 - 9:00'),
+        ('9:00 - 10:00'),
+        ('10:00 - 11:00'),
+        ('11:00 - 12:00'),
+        ('12:00 - 13:00'),
+        ('13:00 - 14:00'),
+        ('14:00 - 15:00'),
+        ('15:00 - 16:00'),
+        ('16:00 - 17:00'),
+        ('17:00 - 18:00'),
+        ('18:00 - 19:00'),
+        ('19:00 - 20:00'),
+        ('20:00 - 21:00')
+    )
     class Meta:
         model = Schedule
         fields = ['name', 'monday', 'tuesday',
